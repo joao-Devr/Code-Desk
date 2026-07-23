f10a3e4a02d170bc322fadbab9f6bad83c339e27
joao-Devr <jpcrodrigues75@gmail.com>
2026-07-23 11:41:47 -0300

    feat: autentificação para entrar no CodeDesk.html

--- CodeDesk/views.py
+++ CodeDesk/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def codedesk(request):
    return render(request, 'CodeDesk.html')
